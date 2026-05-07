f64e2f31fb9c682ab24343aee21bd296da3376f4
ArijolaKrasniqi13 <arijola.krasniqi@student.uni-pr.edu>
2026-05-07 15:31:26 +0200

    krijimi i inverse matrix dhe perfundimi i main

--- main.py
+++ main.py
@@ -11,3 +11,19 @@ while True:
     print("1. Encrypt")
     print("2. Decrypt")
     print("3. Exit")
+
+    choice = input("Zgjedh opsionin: ")
+
+    if choice == "1":
+        text = input("Shkruaj tekstin: ")
+        print("Encrypted:", encrypt(text, key_matrix))
+
+    elif choice == "2":
+        text = input("Shkruaj ciphertext: ")
+        print("Decrypted:", decrypt(text, key_matrix))
+
+    elif choice == "3":
+        break
+
+    else:
+        print("Opsion i pavlefshem!")
